a4835586f9ed0ef7c0d26c48fee90c3ddb2950d6
averondrv <daniildaniilov20155@gmail.com>
2025-12-16 21:17:11 +0200

    bodr2

--- mtriz.java
+++ mtriz.java
@@ -1,7 +1,62 @@
 class mtlab1 {
     public static void main(String[] args) {
-    System.out.println("Its works");
-    }
+        System.out.println("Program works");
 
-// This code was created by student
+        int levels = 7; // было 6
+        System.out.println("\n1) Ялинка у вигляді піраміди:");
+
+        Piramid piramid = new Piramid();   
+        piramid.printTree(levels);
+
+        System.out.println("\n2) Двовимірний масив:");
+        int rows = 3;
+        int cols = 5;
+        int startValue = 1; // было 0
+        int step = 2;       // было 3
+
+        Array2DGenerator generator = new Array2DGenerator();
+        generator.Array2D(rows, cols, startValue, step);
+    }
+}
+
+class Piramid {
+    public void printTree(int levels) {
+        for (int i = 0; i < levels; i++) {
+            int stars = 2 * i + 1;
+            int spaces = levels - i - 1;
+
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" "); // было "_"
+            }
+
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class Array2DGenerator {
+    public int[][] Array2D(int rows, int cols, int startValue, int step) {
+        int[][] arr = new int[rows][cols];
+        int value = startValue;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = value;
+                value += step;
+            }
+        }
+
+        for (int[] row : arr) {
+            for (int num : row) {
+                System.out.printf("%6d", num); // было %8d
+            }
+            System.out.println();
+        }
+
+        return arr;
+    }
 }
